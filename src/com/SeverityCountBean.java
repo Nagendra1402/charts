1994bf56ee39865c3019236d555823e050289edb
Nagendra1402 <nag1402@gmail.com>
2014-07-21 12:15:02 +0530

    Create SeverityCountBean.java

---
+++ src/com/SeverityCountBean.java
@@ -0,0 +1,19 @@
+package com.fid.collab;
+
+public class SeverityCountBean {
+
+	private int severityCount;
+	private String assignmentGroup;
+	public int getSeverityCount() {
+		return severityCount;
+	}
+	public void setSeverityCount(int severityCount) {
+		this.severityCount = severityCount;
+	}
+	public String getAssignmentGroup() {
+		return assignmentGroup;
+	}
+	public void setAssignmentGroup(String assignmentGroup) {
+		this.assignmentGroup = assignmentGroup;
+	}
+}
