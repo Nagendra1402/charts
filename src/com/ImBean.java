cf0b7d7fa7ab95d0d9adbfdd892fa2c06ae8fe1a
Nagendra1402 <nag1402@gmail.com>
2014-07-21 12:13:07 +0530

    Create ImBean.java

---
+++ src/com/ImBean.java
@@ -0,0 +1,40 @@
+package com.fid.collab;
+
+public class ImBean {
+
+	private String imNumber;
+	private String assignmentGroup;
+	private String assigneeName;
+	private String description;
+	private String severity;
+	public String getImNumber() {
+		return imNumber;
+	}
+	public void setImNumber(String imNumber) {
+		this.imNumber = imNumber;
+	}
+	public String getAssignmentGroup() {
+		return assignmentGroup;
+	}
+	public void setAssignmentGroup(String assignmentGroup) {
+		this.assignmentGroup = assignmentGroup;
+	}
+	public String getAssigneeName() {
+		return assigneeName;
+	}
+	public void setAssigneeName(String assigneeName) {
+		this.assigneeName = assigneeName;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public String getSeverity() {
+		return severity;
+	}
+	public void setSeverity(String severity) {
+		this.severity = severity;
+	}
+}
