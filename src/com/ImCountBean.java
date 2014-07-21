88dd35f86563013f47fcadd87ec0a0d32a314b81
Nagendra1402 <nag1402@gmail.com>
2014-07-21 12:13:38 +0530

    Create ImCountBean.java

---
+++ src/com/ImCountBean.java
@@ -0,0 +1,18 @@
+package com.fid.collab;
+
+public class ImCountBean {
+private int imCount;
+private String assginmentGroup;
+public int getImCount() {
+	return imCount;
+}
+public void setImCount(int imCount) {
+	this.imCount = imCount;
+}
+public String getAssginmentGroup() {
+	return assginmentGroup;
+}
+public void setAssginmentGroup(String assginmentGroup) {
+	this.assginmentGroup = assginmentGroup;
+}
+}
